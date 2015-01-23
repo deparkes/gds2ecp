a00a590c8fceba791207b2e2eba2261f35f2e269
Duncan Parkes <ppxdep@nottingham.ac.uk>
2015-01-23 13:56:13 +0000

    Strip out xy into groups of 5, defining shapes

--- gds2ecp.py
+++ gds2ecp.py
@@ -1,10 +1,19 @@
+import itertools
+
+def grouper(iterable, n, fillvalue=None):
+    args = [iter(iterable)] * n
+    return itertools.izip_longest(*args, fillvalue=fillvalue)
 
 def getxy(gds_file):
     with open(gds_file) as f:
         for line in f:
             if line.startswith('XY: '):
                 line = line.strip('XY: ')
-                print(line)
+                line = line.split()
+                return line
                 
+                
+#print (getxy('writefield.txt'))
 
-getxy('writefield.txt')
+grouped = grouper(getxy('writefield.txt'),5)
+print list(grouped)
