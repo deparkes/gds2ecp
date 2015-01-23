8886287890c8907649467d228d40527145bbcf58
Duncan Parkes <ppxdep@nottingham.ac.uk>
2015-01-23 14:33:07 +0000

    Remove comma and newline characters from xy list

--- gds2ecp.py
+++ gds2ecp.py
@@ -1,4 +1,5 @@
 import itertools
+import csv
 
 def grouper(iterable, n, fillvalue=None):
     args = [iter(iterable)] * n
@@ -9,11 +10,20 @@ def getxy(gds_file):
         for line in f:
             if line.startswith('XY: '):
                 line = line.strip('XY: ')
-                line = line.split()
+                line = line.strip('\n')
+                line = line.split(', ')
                 return line
                 
+def topat(coords):
+    """ Do this on a per-shape basis """
+    for chunk in coords:
+        xy_pairs = grouper(chunk,2)
+        print list(xy_pairs)
                 
 #print (getxy('writefield.txt'))
 
-grouped = grouper(getxy('writefield.txt'),5)
-print list(grouped)
+grouped = grouper(getxy('writefield.txt'),10)
+#print list(grouped)
+
+topat(list(grouped))
+
